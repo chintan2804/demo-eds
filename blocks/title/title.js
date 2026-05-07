652ea399b38403f80617e29810aabb4515ca6aae
chintan2804 <chintan280495@gmail.com>
2026-05-07 15:22:05 +0530

    Updating Title Block

--- blocks/title/title.js
+++ blocks/title/title.js
@@ -1,12 +1,4 @@
-export default function decorate(block) {
-  // The block will have two rows: first is the block name, second is the title text
-  const titleRow = block.children[1]; // 0 is block name, 1 is the title
-  if (titleRow) {
-    const titleText = titleRow.textContent.trim();
-    block.innerHTML = ''; // Clear block
-    const h1 = document.createElement('h1');
-    h1.className = 'title-block-title';
-    h1.textContent = titleText;
-    block.appendChild(h1);
-  }
+export default function decorate() {
+  const titleDiv = document.querySelector('.title div');
+
 }
